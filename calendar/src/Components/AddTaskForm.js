49cdf78a4de1f4f06d60a2e05bca3664b6393379
codejoncode <jonathanjholloway@gmail.com>
2018-08-05 11:57:09 -0500

    added the description and title and add task button

--- calendar/src/Components/AddTaskForm.js
+++ calendar/src/Components/AddTaskForm.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import {Form, Input, FormGroup, Label} from 'reactstrap';
+import {Form, Input, FormGroup} from 'reactstrap';
 import styled from 'styled-components';
 
 const FormContainer = styled.div`
     margin: 10px;
 `;
-
+const Label = styled.label`
+    font-size: 1vw;
+`;
 
 class AddTaskForm extends React.Component{
     constructor(props){
@@ -20,12 +22,21 @@ class AddTaskForm extends React.Component{
             <FormContainer>
                 <Form>
                     <FormGroup>
+                        <Label for ="titleText">Title</Label>
+                        <br/>
+                        <Input type="textarea" name="text" id="titleText" />
+                        <br/>
+                    </FormGroup>
+                    <FormGroup>
                     <Label for = "descriptionText">Description</Label>
                     <br/>
                     <Input type="textarea" name="text" id="descriptionText" />
                     </FormGroup>
+                    <br/>
+                    <button>Add Task</button>
                 </Form>
             </FormContainer>
+            
         );
     }
 }
